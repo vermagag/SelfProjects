c1ce8825b5332550ec000a3a656bb9e3800fab31
atitest <vermagag@gmail.com>
2016-03-08 13:16:39 +0530

    Added 5 les

---
+++ rebooter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+from time import sleep,ctime
+import sys,os
+
+def main():
+   RebooterLog = open("/home/atitest/Rebooter_Log.txt",'a')
+   
+   if os.path.isfile("/home/atitest/Rebooter_Count.txt") == False:
+      if len(sys.argv)>1:
+         NumberOfReboots = int(sys.argv[1])
+      else:
+         NumberOfReboots = int(raw_input("Please Enter the number of Reboots: "))
+      RebooterLog.write(str(ctime())+" Test Name : REBOOTER "+str(NumberOfReboots)+'\n')
+      CountFD = open("/home/atitest/Rebooter_Count.txt",'w')
+      CountFD.write(str(NumberOfReboots))
+      CountFD.close()
+   else:
+      NumberOfReboots = int(open("/home/atitest/Rebooter_Count.txt").read())
+      print(str(NumberOfReboots)+" cycles more to reboot")
+      RebooterLog.write(str(NumberOfReboots)+" cycles more to reboot\n")
+      NumberOfReboots = NumberOfReboots -1
+      CountFD = open("/home/atitest/Rebooter_Count.txt",'w')
+      CountFD.write(str(NumberOfReboots))
+      CountFD.close()
+   if NumberOfReboots >0:
+      RebooterLog.close()
+      sleep(60)
+      os.system("mpv -vo vdpau -hwdec=vdpau /home/atitest/Files/qwe.mp4 &")
+      sleep(20)
+      os.system("reboot")
+   else:
+      RebooterLog.write(str(ctime())+" Test Finished Successfully\n")
+      RebooterLog.close()
+      print("Rebooter Test Finished Successfully!\n")
+      sys.exit(0)
+
+if __name__ == "__main__":
+   main()
